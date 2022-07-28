3e9085b8c973b89fe57e4d0d8a48fe4a4f21c480
Replit user <>
2022-07-28 11:55:30 +0000

    Fixed + added gitignore for replit

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 def monthly_compounding(initial, apr, monthly, years):
     final_sum = initial
     apr = apr/100
-    monthly_rate = pow(1 + apr, 1/13) - 1
+    monthly_rate = pow(1 + apr, 1/12) - 1
     for _ in range(0, years*12):
         final_sum = final_sum*(1 + monthly_rate) + monthly
 
